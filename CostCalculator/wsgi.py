82335eb5e05e8324d4c5d6d720a33f95eb06ff59
folong-zidane <tafoukeuz@gmail.com>
2024-12-13 09:43:53 +0100

    ok

--- CostCalculator/wsgi.py
+++ CostCalculator/wsgi.py
@@ -1,5 +1,16 @@
+"""
+WSGI config for CostCalculator project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CostCalculatorcd .settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CostCalculator.settings')
+
 application = get_wsgi_application()
