f7a73c7b03da5dbd13f7ceba1ab1bb2e95b22c03
folong-zidane <tafoukeuz@gmail.com>
2024-12-13 07:07:35 +0100

    "first commit"

--- CostCalculator/setting.py
+++ CostCalculator/setting.py
@@ -1,1 +1,7 @@
 ALLOWED_HOSTS = ['.vercel.app']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Ajoutez ceci
+    ...
+]
